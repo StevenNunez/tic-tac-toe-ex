d97a6424c413001128999a144936f91d0db48a3e
Steven Nunez <steven.nunez@gmail.com>
2017-09-08 21:33:02 -0400

    Create Runner to start game

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -1,18 +1,42 @@
 defmodule TicTacToe do
-  @moduledoc """
-  Documentation for TicTacToe.
-  """
+  alias TicTacToe.Game
+  alias TicTacToe.CLIPresenter
+  def start do
+    {:ok, game} = Game.new
+    welcome
+    play(game)
+  end
 
-  @doc """
-  Hello world.
+  def welcome do
+    IO.puts "Welcome to tic-tac-toe!"
+  end
 
-  ## Examples
+  def play(game) do
+    CLIPresenter.display(game)
+    choice = IO.gets "It's #{game.current_player}'s turn. Where would you like to play?"
+    case Integer.parse(choice) do
+      {position, _} ->
+        case Game.play_move(game, position) do
+          {:ok, game} -> play(game)
+          {:error, :spot_taken, _game} ->
+            IO.puts "Looks like that spot is taken. Try again"
+            play(game)
+        end
+    :error ->
+      IO.puts "Invalid Input. Try again"
+      play(game)
+    end
+  end
+end
 
-      iex> TicTacToe.hello
-      :world
+defmodule TicTacToe.CLIPresenter do
+  alias __MODULE__
 
-  """
-  def hello do
-    :world
+  def display(game) do
+
+    Enum.chunk(game.board.positions, 3)
+    |> Enum.each(fn (row) ->
+      IO.puts Enum.join(row, " | ") <> "\n"
+    end)
   end
 end
